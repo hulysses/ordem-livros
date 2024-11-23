d2f58a0e237a78be2715ab187a48f9696d6b9502
hulysses <hulyssesmaga@gmail.com>
2024-11-23 12:42:17 -0300

    feat: add Logo component and update layout structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import "../app.css";
+  let { children } = $props();
+  import Logo from "../components/template/Logo.svelte";
 </script>
 
-{@render children()}
+<div
+  class="bg-black flex flex-col justify-center items-center min-h-screen text-white"
+>
+  <Logo />
+  {@render children()}
+</div>
