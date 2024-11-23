c098fab2f073519e6b4561fe2cbd3e5a222d0fbd
hulysses <hulyssesmaga@gmail.com>
2024-11-23 18:27:24 -0300

    feat: implement series detail page with dynamic content and layout

---
+++ src/components/serie/ConteudoSerie.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let serie: any = null;
+</script>
+
+{#if serie}
+  <div class="flex flex-col gap-5">
+    <h1 class="text-3xl font-bold">{serie.titulo}</h1>
+    <a href="/autores/{serie.autor.id}" class="text-sm text-zinc-400 -mt-4">
+      {serie.autor.nome}
+    </a>
+    <img src={serie.imagem} alt="Imagem da série" class="h-96 object-cover" />
+    <p class="text-lg text-zinc-400 text-justify">{serie.descricao}</p>
+    <h2>Lista de livros</h2>
+    {#each serie.livros as livro}
+      <div>{livro.titulo}</div>
+    {/each}
+  </div>
+{/if}
---
+++ src/routes/series/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import ItemSerie from "../../components/serie/ItemSerie.svelte";
+  import Titulo from "../../components/shared/Titulo.svelte";
+  import series from "../../constants/series";
+</script>
+
+<div class="flex flex-col gap-10">
+  <Titulo
+    tituloPrincipal="Autores"
+    tituloSecundario="Conheça os autores das histórias que você ama."
+  />
+
+  <div class="grid grid-cols-2 gap-5">
+    {#each series as serie}
+      <ItemSerie {serie} />
+    {/each}
+  </div>
+</div>
