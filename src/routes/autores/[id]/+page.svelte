15609faad9cb83ea36815f10ffeefd38cd0a83ce
hulysses <hulyssesmaga@gmail.com>
2024-11-23 17:52:40 -0300

    feat: remove individual author pages and implement dynamic routing for author content

--- src/routes/autores/[id]/+page.svelte
+++ src/routes/autores/[id]/+page.svelte
@@ -1,6 +1,6 @@
 <script>
+  import { page } from "$app/stores";
   import ConteudoAutor from "../../../components/autor/ConteudoAutor.svelte";
-  import autores from "../../../constants/autores";
 </script>
 
-<ConteudoAutor autor={autores[1]} />
+<ConteudoAutor autor={$page.data.autor} />
