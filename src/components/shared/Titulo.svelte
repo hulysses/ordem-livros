d07e66f2fa60685fa638ea9cdcdfb6df944c8313
hulysses <hulyssesmaga@gmail.com>
2024-11-23 17:28:47 -0300

    feat: add Titulo component for author page header and update layout structure

---
+++ src/components/shared/Titulo.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let tituloPrincipal = "";
+  export let tituloSecundario = "";
+</script>
+
+<div class="flex flex-col">
+  <h1 class="text-3xl font-bold">{tituloPrincipal}</h1>
+  <p class="text-sm text-zinc-400">{tituloSecundario}</p>
+</div>
--- src/routes/autores/+page.svelte
+++ src/routes/autores/+page.svelte
@@ -1,10 +1,18 @@
 <script>
   import ItemAutor from "../../components/autor/ItemAutor.svelte";
+  import Titulo from "../../components/shared/Titulo.svelte";
   import autores from "../../constants/autores";
 </script>
 
-<div class="grid grid-cols-2 gap-5">
-  {#each autores as autor}
-    <ItemAutor {autor} />
-  {/each}
+<div class="flex flex-col gap-10">
+  <Titulo
+    tituloPrincipal="Autores"
+    tituloSecundario="Conheça os autores das histórias que você ama."
+  />
+
+  <div class="grid grid-cols-2 gap-5">
+    {#each autores as autor}
+      <ItemAutor {autor} />
+    {/each}
+  </div>
 </div>
