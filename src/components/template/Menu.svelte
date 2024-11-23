44fe418d43b3007d39ceefe0791d14ac85a3446b
hulysses <hulyssesmaga@gmail.com>
2024-11-23 15:41:54 -0300

    feat: implement Menu and MenuItem components, enhance Rodape styling, and add MenuItens for better navigation structure

--- src/components/template/Menu.svelte
+++ src/components/template/Menu.svelte
@@ -1,3 +1,27 @@
+<script>
+  import MenuItem from "./MenuItem.svelte";
+  import MenuItens from "./MenuItens.svelte";
+</script>
+
 <aside
   class="flex flex-col gap-10 self-start border border-zinc-800 p-5 rounded-md w-72"
-></aside>
+>
+  <MenuItens titulo="Navegação">
+    <MenuItem texto="Início" />
+    <MenuItem texto="Personagens" />
+    <MenuItem texto="Autores" />
+    <MenuItem texto="Séries" />
+  </MenuItens>
+  <MenuItens titulo="Principais autores">
+    <MenuItem texto="Akira Toriyama" />
+    <MenuItem texto="Eiichiro Oda" />
+    <MenuItem texto="Masashi Kishimoto" />
+  </MenuItens>
+  <MenuItens titulo="Outras categorias">
+    <MenuItem texto="Últimas notícias" />
+    <MenuItem texto="Séries populares" />
+    <MenuItem texto="Facebook" />
+    <MenuItem texto="Instagram" />
+    <MenuItem texto="YouTube" />
+  </MenuItens>
+</aside>
---
+++ src/components/template/MenuItem.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let url = "/";
+  export let texto = "";
+</script>
+
+<li>
+  <a
+    href={url}
+    class="flex gap-2 items-center cursor-pointer px-4 py-2 text-zinc-300
+    hover:underline hover:text-white hover:bg-white/10"
+  >
+    <span>{texto}</span>
+  </a>
+</li>
+ 
