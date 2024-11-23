3f0ed7b8aea18412413aae3c47974c7a842cb0b9
hulysses <hulyssesmaga@gmail.com>
2024-11-23 18:07:30 -0300

    feat: update logo to be a link and implement series listing with ItemSerie component

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import ItemSerie from "../components/serie/ItemSerie.svelte";
+  import series from "../constants/series";
+</script>
+
+<div class="flex flex-col gap-5">
+  {#each series as serie}
+    <div>
+      <ItemSerie {serie} />
+    </div>
+  {/each}
+</div>
